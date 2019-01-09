bb975427b23048c6b0007bf7a2325b6d31a40547
Katharina Treptow <katharina.treptow@signal-iduna.de>
2019-01-09 09:34:43 +0100

    trim.bash script edited

--- trim.bash
+++ trim.bash
@@ -6,22 +6,14 @@ cd svg
 for name in *.svg; do
 	name_png=$(echo $name | sed 's/svg/png/ig')
 	inkscape $name -w $WIDTH --export-background="#000000"  --export-png=$PWD_BASE/png/black-background/$name_png
-	inkscape $name -w $WIDTH --export-background="#ffffff"  --export-png=$PWD_BASE/png/white-background/$name_png
 	inkscape $name -w $WIDTH --export-png=$PWD_BASE/png/no-background/$name_png
-#	inkscape $name -w $WIDTH --export-background="#ffffff"  --export-png=$name_png
-#	inkscape $name -w $WIDTH --export-png=$name_png
 done
 cd ..
 
 cd png
 for name in $(find . | grep ".png$"); do
-	trimed_name="tmp.png"
-#	convert $name -layers flatten -background red  -alpha on  -fuzz 1%  -trim +repage $trimed_name; 
-        convert $name -fuzz 1%  -trim +repage -flatten -quality 100 -colorspace CMYK $trimed_name;
-#	convert $name -flatten -fuzz 1% -trim +repage $trimed_name; 
 	resized_name="tmp2.png"
-	convert -resize $WIDTH"x$WIDTH" "$trimed_name" $resized_name; 
+	convert -resize $WIDTH"x$WIDTH" "$name" $resized_name; 
 	mv $resized_name $name
-	rm $trimed_name
 done
 cd ..
